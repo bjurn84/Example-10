f6e8b82c19518d9f4e6b726f698be876b895a5d7
bjurn84 <115032837+bjurn84@users.noreply.github.com>
2022-11-16 17:36:41 +0400

    Work10

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Сейчас будет вторая цифра этого числа -> "+stringNumber[1]);
